063c6aefd96c1bacbf53639704832a65c2937549
rian <rianrbps@gmail.com>
2025-02-03 01:42:05 -0800

    explicação da iteração circular usando %

--- 1752.py
+++ 1752.py
@@ -9,6 +9,8 @@ class Solution(object):
 
         # contando quantas vezes a ordem quebra
         for i in range(n):
+            # nums[(i+1)%n] torna a comparação circular, ou seja o último elemento vai poder ser comparado com o primeiro
+            # se precisar de referência futura o rian do passado escreveu a equação com exemplo no caderno
             if nums[i] > nums[(i+1)%n]:
                 cont += 1
         
